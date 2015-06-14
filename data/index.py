3523cd4ea09028d35185f385c418c1d783256539
Pablo <pabloromanh@gmail.com>
2015-06-14 09:24:40 +0200

    Updated CEDICT to latest version and wrote script to generate index file.

---
+++ data/index.py
@@ -0,0 +1,52 @@
+
+def utf8len(s):
+    return len(s.decode('utf-8'))
+
+cedict = open('cedict_ts.u8')
+
+line = cedict.readline()
+
+byte = 0
+
+dictionary = {}
+
+def saveToDict(key, value):
+    dictionary.setdefault(key, [])
+    dictionary[key].append(str(value))
+
+while line:
+    while line[0] == '#':
+        byte += utf8len(line)
+        line = cedict.readline()
+
+    spacepos = line.find(' ')
+    firstentry = line[0:spacepos]
+    spacepos2 = line.find(' ', spacepos + 1)
+    secondentry = line[spacepos + 1: spacepos2]
+    if firstentry == secondentry:
+        saveToDict(firstentry, byte)
+    else:
+        saveToDict(firstentry, byte)
+        saveToDict(secondentry, byte)
+
+    byte += utf8len(line)
+    line = cedict.readline()
+
+cedict.close()
+
+
+outputlist = []
+
+for key in dictionary:
+    newline = key
+    for entry in dictionary[key]:
+        newline += ',' + entry
+    newline += '\n'
+    outputlist.append(newline)
+
+outputlist.sort()
+
+index = open('cedict.idx', 'w')
+index.writelines(outputlist)
+
+index.close()
